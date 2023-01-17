c002483666e4d3e35407387ef814f3d71939ee88
MiguelBelo95 <belo_miguel@hotmail.com>
2023-01-17 20:24:15 +0000

    Add Grid creation function and update grid onmousemove event

--- resources/logic.js
+++ resources/logic.js
@@ -1,3 +1,24 @@
+const defaultSize = 15;
+const defaultMode = 'color';
+const defaultColor = '#333333';
+
+let currentSize = defaultSize;
+let currentMode = defaultMode;
+let currentColor = defaultColor;
+
+function updateSize(newSize) {
+	currentSize = newSize;
+}
+
+function updateMode(newMode) {
+	//activateButton(newMode);
+	currentMode = newMode;
+}
+
+function updateColor(newColor) {
+	currentColor = newColor;
+}
+
 const color = document.getElementById('colorSelector');
 const penBtn = document.getElementById('penBtn');
 const rainbowBtn = document.getElementById('rainbowBtn');
@@ -7,20 +28,61 @@ const eraserBtn = document.getElementById('eraserBtn');
 const clearBtn = document.getElementById('clearBtn');
 const sizeDisplay = document.getElementById('sizeValue');
 const sizeSlider = document.getElementById('grid-size');
+const grid = document.getElementById('gridbox');
 
-sizeDisplay.textContent = `${sizeSlider.value} X ${sizeSlider.value}`;
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
-//GRID SIZE
-sizeDisplay.textContent = `${sizeSlider.value} X ${sizeSlider.value}`;
+let gridSize = sizeSlider.value;
 
-function changeValue() {
-	sizeDisplay.textContent = "" ;
-	let gridSize = sizeSlider.value;
-	sizeDisplay.append(`${gridSize} X ${gridSize}`);
+sizeSlider.onchange = (e) => updateSizeValue(e.target.vaule);
+sizeSlider.onchange = (e) => changeSize(e.target.value);
+
+
+//Update GRID size on any range change on the slider button
+function changeSize(size) {
+	updateSize(size);
+	updateSizeValue(size);
+	reloadGrid();
 }
+
+function updateSizeValue(value) {
+	sizeDisplay.innerHTML = `${value} x ${value}`;
+}
+
+function clearGrid() {
+	grid.innerHTML = "";
+}
+
+function reloadGrid() {
+	clearGrid();
+	createGrid(currentSize);
+}
+
+//GRID
+function createGrid(size) {
+	grid.style.gridTemplateColumns = `grid-template-columns: repeat(${size}, 1fr)`;
+	grid.style.gridTemplateRows = `grid-template-rows: repeat(${size}, 1fr);`
+	
+	for (let i=0; i < size * size; i++) {
+		const gridCell = document.createElement('div');
+		gridCell.classList.add('grid-cell');
+		//gridCell.addEventListener('mouseover', changeColor);
+		//gridCell.addEventListener('mousedown', changeColor);
+		grid.appendChild(gridCell);
+	}
+}
+
 
 //Toggle active and inactive on buttons, change background of btn
 
 
 //Add EventListeners to the buttons;
 
+
+//AUTOMATIC LAUNCH SETUP
+window.onload = () => {
+	createGrid(defaultSize);
+	//activateButton(defaultMode);
+}
